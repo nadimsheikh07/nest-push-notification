c59ecc0d2c4176945d434aee37d9827322b450c1
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-03-05 15:29:47 +0530

    fix notification

--- src/notification/dto/update-notification.dto.ts
+++ src/notification/dto/update-notification.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateNotificationDto } from './create-notification.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateNotificationDto extends PartialType(CreateNotificationDto) {}
+export class UpdateNotificationDto extends PartialType(CreateNotificationDto) {
+  @IsOptional()
+  isRead?: boolean;
+}
--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import {
+  Notification,
+  NotificationSchema,
+} from './entities/notification.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Notification.name, schema: NotificationSchema },
+    ]),
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
+  exports: [NotificationService],
 })
 export class NotificationModule {}
