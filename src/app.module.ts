a8eec6352d07909395d813b1a03c77219e01a8be
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-03-05 15:19:19 +0530

    seup env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenModule } from './token/token.module';
 import { NotificationModule } from './notification/notification.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TokenModule, NotificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // use env anywhere
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+    }),
+    TokenModule,
+    NotificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable global validation
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      whitelist: true, // remove extra fields
+      forbidNonWhitelisted: true, // throw error on unknown fields
+      transform: true, // auto-transform payloads to DTO instances
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Enable CORS if frontend apps will call API
+  app.enableCors();
+
+  // Use ConfigService to get PORT
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
